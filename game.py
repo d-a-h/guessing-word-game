0bdc02fa9792ce8072740c07732653332b738130
Arshad <134198615+d-a-h@users.noreply.github.com>
2023-07-18 17:01:07 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+secret_word = 'arshad'
+guess = ''
+
+guess_limit = 0
+while guess != secret_word  and guess_limit < 3:
+    guess = input('enter ur guess : ')
+    
+    guess_limit += 1
+    if secret_word == 'guess':
+        print('you won')
+else:
+    print('you lost the game')
+
