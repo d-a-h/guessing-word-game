7f184f3d3034d7fac3042edc3a80fcd0e7caa569
Arshad <134198615+d-a-h@users.noreply.github.com>
2023-07-19 12:30:54 +0530

    game.py

--- game.py
+++ game.py
@@ -6,8 +6,9 @@ while guess != secret_word  and guess_limit < 3:
     guess = input('enter ur guess : ')
     
     guess_limit += 1
-    if secret_word == 'guess':
+    if secret_word == guess:
         print('you won')
-else:
+        break
+    else:
     print('you lost the game')
 
